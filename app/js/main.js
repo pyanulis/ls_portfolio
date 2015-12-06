5d456db1424e21a8cf09623976ab1ff9b76dd7de
pyanulis <pyanulis@gmail.com>
2015-12-06 20:45:56 +0300

    ie8, fonts

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,5 @@
-$(function(){
-    return;
-    
-    console.log("js loaded");
-    
-    var classMenuSelected = "menuItemSelected";
-    var prevMenuSelected = $("." + classMenuSelected)[0];
-    var prevent = true;
-    
-    $(".menuItem").on("click", function(e){
-        
-        if (!prevent){
-            console.log("trigger");
-            prevent = true;
-            return;   
-        }
-        
-        e.preventDefault();
-        
-        console.log("custom");
-        
-        var il = e.currentTarget;
-        
-        $(il).toggleClass(classMenuSelected);
-        
-        if (prevMenuSelected){
-            $(prevMenuSelected).toggleClass(classMenuSelected);
-        }
-        
-        prevMenuSelected = il;
-        
-        var a = $(il).children()[0];
-        
-        prevent = false;
-        $(a).trigger("click");
-    })
+$(document).ready(function(){
+    if (!Modernizr.input.placeholder) {
+        $("input, textarea").placeholder();
+    }
 });
