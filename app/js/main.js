6891594506f8baf5830fa4bf9c1f35f3771bfbad
pyanulis <pyanulis@gmail.com>
2015-12-15 23:18:32 +0300

    popup: upload js

--- app/js/main.js
+++ app/js/main.js
@@ -10,14 +10,48 @@ var myModule = (function(){
         setupListeners();
     };
     
+    var inputImage = $("#imageUpload");
+    
     var setupListeners = function(){
         $("#add-project-link").on("click", showAddProjectPopup);
+        
+        $(".icoClose").on("click", closePopup);
+        
+        inputImage.on("change", uploadChange);
     };
     
     var showAddProjectPopup = function(e){
         console.log("popup call");
         e.preventDefault();
         $("#add-project-popup").bPopup();
+    };
+    
+    var closePopup = function(e){
+        console.log("popup close");
+        e.preventDefault();
+        $("#add-project-popup").bPopup().close();
+    };
+    
+    var uploadChange = function(){
+        console.log("image upload");
+        
+        var file_api = window.File && window.FileReader && window.FileList && window.Blob;
+        var file_name;
+        var inputImagePath = $("#imageFilePath");
+        
+        console.log(file_api);
+        
+        if(file_api && inputImage[0].files[0]) 
+            file_name = inputImage[0].files[0].name;
+        else
+            file_name = inputImage.val();
+
+        console.log(file_name);
+        
+        if(!file_name.length)
+            return;
+        
+        inputImagePath.val(file_name);
     };
     
     return {
