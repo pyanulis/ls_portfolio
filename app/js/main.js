56091099755f129a6ccbdd07d883a0e7e7a1c09a
pyanulis <pyanulis@gmail.com>
2015-12-15 05:27:21 +0300

    popup

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,27 @@ $(document).ready(function(){
         $("input, textarea").placeholder();
     }
 });
+
+var myModule = (function(){
+    
+    var init = function(){
+        setupListeners();
+    };
+    
+    var setupListeners = function(){
+        $("#add-project-link").on("click", showAddProjectPopup);
+    };
+    
+    var showAddProjectPopup = function(e){
+        console.log("popup call");
+        e.preventDefault();
+        $("#add-project-popup").bPopup();
+    };
+    
+    return {
+        init: init
+    };
+    
+})();
+
+myModule.init();
