e209177934f62732e24932e26a87f9114f9c5e23
pyanulis <pyanulis@gmail.com>
2015-12-16 05:14:03 +0300

    popup: validate

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
+    
+    /*Modernizr*/
     if (!Modernizr.input.placeholder) {
         $("input, textarea").placeholder();
     }
-});
-
-var myModule = (function(){
     
-    var init = function(){
-        setupListeners();
-    };
+    var errorClassName = "inputError";
+    var formAddProject = $("#formAddProjectId");
+    var validateFailed = false;
+    var addProjectFilter = "input[name=projectTitle], input[name=projectImagePath], input[name=projectUrl], textarea[name=projectDescription]";
     
     var inputImage = $("#imageUpload");
     
@@ -18,19 +18,37 @@ var myModule = (function(){
         $(".icoClose").on("click", closePopup);
         
         inputImage.on("change", uploadChange);
+        
+        formAddProject.on("submit", checkAddProject);
     };
     
     var showAddProjectPopup = function(e){
         console.log("popup call");
         e.preventDefault();
-        $("#add-project-popup").bPopup();
+        $("#add-project-popup").bPopup({
+            onClose: function(){
+                        console.log('closed');
+                        clearAddProjectForm();
+                   }
+        });
     };
     
     var closePopup = function(e){
         console.log("popup close");
         e.preventDefault();
+        
+        clearAddProjectForm();
         $("#add-project-popup").bPopup().close();
     };
+    
+    var clearAddProjectForm = function(){
+        formAddProject[0].reset();
+        var inputs = formAddProject.find(addProjectFilter);
+        
+        inputs.each(function(index){
+            clearError($(this));
+        });
+    }
     
     var uploadChange = function(){
         console.log("image upload");
@@ -54,10 +72,51 @@ var myModule = (function(){
         inputImagePath.val(file_name);
     };
     
-    return {
-        init: init
-    };
+    var checkAddProject = function(e){
+        console.log("submit add project check");
+        e.preventDefault();
+        
+        var form = $(this);
+        
+        var inputs = form.find(addProjectFilter);
+        
+        inputs.each(function(index){
+            
+            var item = $(this);
+            
+            if (!item.val()){
+                console.log(item);
+                
+                item.addClass(errorClassName);
+                item.on("input", removeErrorClass);
+                
+                validateFailed = true;
+                
+                /* doesn't work yet */
+                /*var tooltipObject = {
+                    content: 'I am positioned using corner values!',
+                    show: {
+                        ready: true
+                    }
+                };
+                console.log(tooltipObject);
+                
+                item.qtip(tooltipObject);*/
+            }
+    });
+    }
     
-})();
-
-myModule.init();
+    var removeErrorClass = function(){
+        console.log("item change");
+        var item = $(this);
+        
+        clearError(item);
+    }
+    
+    var clearError = function(item){
+        item.removeClass(errorClassName);
+        item.off("input", removeErrorClass);
+    }
+    
+    setupListeners();
+});
